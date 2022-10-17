5c7840479cd51c1ef11332d6b1cfa7fba27c096f
teuzet <teuzet92@gmail.com>
2022-10-17 10:26:46 +0200

    summoner-main server

--- gameServer/core.js
+++ gameServer/core.js
@@ -2,6 +2,7 @@ const Master = {};
 
 Master.onCreate = function(data) {
     this.create(GameServer, { id: 'game_polygon', gameId: 'game_polygon' });
+    this.create(GameServer, { id: 'game_summoner-main', gameId: 'game_summoner-main' });
 }
 
 module.exports = Master

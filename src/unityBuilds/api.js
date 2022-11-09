90d47d7b49bbbd59c8fcef057ad564f93d7ae1d2
teuzet <teuzet92@gmail.com>
2022-11-09 13:26:33 +0100

    getUnityBuiild command fix

--- src/unityBuilds/api.js
+++ src/unityBuilds/api.js
@@ -1,5 +1,5 @@
 const { ObjectId } = require('mongodb');
-const Master = { api: }
+const Master = { api: {} }
 
 Master.api.getUnityBuild = async function(params) {
       let res = await this.core.solceryDb.objects.findOne({ _id: ObjectId(params.buildId) });
